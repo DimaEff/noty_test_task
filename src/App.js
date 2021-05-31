20d712be03aa8d885bf9532ed4dd918865c2d232
DimaEff <fominienkovd@users.noreply.github.com>
2021-05-31 11:42:20 +0300

    app.js fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import {BrowserRouter, Link} from 'react-router-dom';
-import {makeStyles, Typography} from "@material-ui/core";
+import {BrowserRouter} from 'react-router-dom';
+import {makeStyles} from "@material-ui/core";
 
 import AppRouter from "./AppRouter";
 import HeaderContainer from "./Components/Header/HeaderContainer";
-import {getProfileRoute, LOGIN_ROUTE} from "./utils/consts";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -20,12 +19,6 @@ const App = () => {
         <BrowserRouter>
             <HeaderContainer />
             <div className={styles.root}>
-                <Typography variant={'h6'}>
-                    <Link to={LOGIN_ROUTE}>login</Link>
-                </Typography>
-                <Typography variant={'h6'}>
-                    <Link to={getProfileRoute()}>profile</Link>
-                </Typography>
                 <AppRouter />
             </div>
         </BrowserRouter>
